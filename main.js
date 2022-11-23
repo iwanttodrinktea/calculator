b2d2e1611a3fdfb96074fafa185188ed669eb1b2
Tomomi Iwasaki <22w2013g@shinshu-u.ac.jp>
2022-11-24 00:01:29 +0900

    change index.html and main.js

--- main.js
+++ main.js
@@ -1,68 +1,63 @@
+//window.addEventListener('load', () => {
+const current = document.querySelector("#current-expression");   // current expression
+const result = document.querySelector("#result");
+
+// all clear
+function clickAc() {
+    if (current.value) {
+        current.value = '';
+    }   
+}
+
+// clear entry
+function clickCe() {
+    if (current.value) {
+        current.value = current.value.slice(0,-1);  //末尾を削除
+    }
+    
+}
+
+// add number or operator
+function append(s) {
+    console.log(s);
+    switch (s){
+        case '+':
+            checkOpe();
+            break;
+        case '-':
+            checkOpe();
+            break;
+        case '*':
+            checkOpe();
+            break;
+        case '/':
+            checkOpe();
+            break;
+        default:
+            // 数字のとき
+    };   
+    current.value += s;
+}
+
+// 入力が記号か判定　記号→前の入力が記号のとき、記号を変更する
+function checkOpe(){
+    if (current.value) {
+        before = current.value.slice(-1);
+        if (!before) {
+            console.log('null');
+        } else if (before==='+' || before==='-' || before==='*' || before==='/') {
+            current.value = current.value.slice(0,-1);
+        }
+    } else {
+        // 入力が無い状態で、計算符号が入力された場合、0を符号の前に加える
+        current.value = 0;
+    }
+    
+}
+
+// calculate
 function calculate() {
     console.log('ans');
-};
-
-
-window.addEventListener('load', () => {
-    const current = document.querySelector("current-expression");   //現在の式
-    const currentOperator = document.querySelector();   //現在の計算記号
-    const beforeOperator = document.querySelector();    //１つ前の計算記号
-    //const input = document.querySelector("#current-expression");
     
-
-
-    function clickAc() {
-        //全部クリア
-        current = '';
-        currentOperator = '';    
-        beforeOperator = '';
-    };
-    
-    function clickCe() {
-        current.value = current.value.slice(0,-1);  //末尾を削除
-    };
-    
-    function append(s) {
-        console.log(s)
-        switch (s){
-            case '+':
-
-        };
-
-        if (s == '+') {
-            ex = ex.slice(0,-1); // 最後尾を削除
-            input.value = input.value.slice(0,-1);
-            input.value += '+';
-        } else if (s == '-') {
-            ex = ex.slice(0,-1); // 最後尾を削除
-            input.value = input.value.slice(0,-1);
-            input.value += '-';
-        } else if (s == '*') {
-            ex = ex.slice(0,-1); // 最後尾を削除
-            input.value = input.value.slice(0,-1);
-            input.value += '×';
-        } else if (s == '/') {
-            ex = ex.slice(0,-1); // 最後尾を削除
-            input.value = input.value.slice(0,-1);
-            input.value += '÷';
-        } else {
-            input.value += s;
-        }
-        // 入力が記号か判定
-        function checkOpe(){
-            if (current==='+' or current==='-' or current==='*' or current==='/') {
-                before = 
-            }
-        };
-        // if (s=='+' or s =='-' or s=='' or s=='') {
-        //     before = True;
-        // } else {
-        //     before = False;
-        // }
-        // // 記号が連続して入力されたら、更新する
-        // if (before) {
-        //     ex = ex.slice(0,-1); // 最後尾を削除
-        // } 
-        ex += s;
-    };
-});
+}
+//});
